269a8c02dcefe2d7959215d38e28430e2c48dc44
latinbox <guaripolo007@gmail.com>
2017-03-09 20:53:27 -0300

    hola

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,5 +83,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'https://aqueous-gorge-58769.herokuapp.com'}
+  config.action_mailer.default_url_options = { :host 'https://aqueous-gorge-58769.herokuapp.com'}
 end
